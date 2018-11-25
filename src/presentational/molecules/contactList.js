4ed97f9e828f986e810f15427ce495cab58feafc
38fanjia <38fanjia+github@gmail.com>
2018-11-25 18:51:17 +0900

    :sparkles: update ContactList

--- src/presentational/molecules/contactList.js
+++ src/presentational/molecules/contactList.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome as home } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter as twitter, faFacebookF as facebook, faInstagram as instagram } from '@fortawesome/free-brands-svg-icons'
 
-// 各リンクの表示方法は変更したい
-export default class extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render () {
+const ContactItem = (items) => {
+  return items.map(({type, url}, index) => {
     return (
-      <ul>
-        <li><a href={this.props.items.url} target="_blank"><FontAwesomeIcon icon={home} /></a></li>
-        <li><a href={this.props.items.twitter} target="_blank"><FontAwesomeIcon icon={twitter} /></a></li>
-        <li><a href={this.props.items.facebook} target="_blank"><FontAwesomeIcon icon={facebook} /></a></li>
-        <li><a href={this.props.items.instagram} target="_blank"><FontAwesomeIcon icon={instagram} /></a></li>
-      </ul>
+      <li key={index}>
+        <a href={url} target="_blank">{type}</a>
+      </li>
     )
-  }
+  })
 }
+
+const ContactList = (props) => {
+  return (
+    <ul>
+      {ContactItem(props.items)}
+    </ul>
+  )
+}
+
+export default ContactList
