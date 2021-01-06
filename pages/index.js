2defa5b23bc8db7160d05e89a18e537c5b51e3bc
fanjia38 <38fanjia@gmail.com>
2021-01-06 16:49:46 +0900

    :+1: next.js 向けに pages/index.js を追加する

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Header from '../src/container/header'
+import Footer from '../src/container/footer'
+import ParadeContainer from '../src/presentational/molecules/paradeContainer'
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <ParadeContainer />
+      <Footer />
+    </>
+  )
+}
+
+export default Index
