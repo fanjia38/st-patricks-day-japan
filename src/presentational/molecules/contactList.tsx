e5129c90b6357f5625ac1e0d2d17afa5d7046c01
fanjia38 <38fanjia@gmail.com>
2021-01-13 17:20:18 +0900

    :+1: add react-icon

--- src/presentational/molecules/contactList.tsx
+++ src/presentational/molecules/contactList.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import { HStack, Link } from '@chakra-ui/react'
+import { HStack, Box, Link, IconButton } from '@chakra-ui/react'
+import { FaHome, FaTwitter, FaFacebookF } from 'react-icons/fa'
 
 function ContactList({items}) {
+  const getIcon = (type) => {
+    switch (type) {
+      case 'home':
+        return <FaHome />
+      case 'twitter':
+        return <FaTwitter />
+      case 'facebook':
+        return <FaFacebookF />
+          }
+    return
+  }
+
   return (
     <HStack>
       {items.map((item, index) => (
-        <Link key={index} href={item.url}>{item.type}</Link>
+        <Box key={index}>
+          <Link key={index} href={item.url}>
+            <IconButton
+              variant="outline"
+              colorScheme="teal"
+              aria-label="web site"
+              icon={getIcon(item.type)}
+            />
+          </Link>
+        </Box>
       ))}
     </HStack>
   )
